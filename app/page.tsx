45dd57ab978c7058d3cdbafa1f342a6bfa330267
JosephRidge <josephridge18@gmail.com>
2025-03-23 20:02:05 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,17 @@ import { CardTile } from "./components/ui/card"
 import { Text } from "./components/ui/text"
 import { Badge } from "./components/ui/badge"
 import {ChatInput} from "./components/ui/chat"
+import { WELCOME_TEXT } from "./lib/constants"
 
 export default function Home() {
   return (
     <div>
       <main>
+        <div>
+          <Text>
+            {WELCOME_TEXT}
+          </Text>
+        </div>
         {/* chat history */}
         <CardTile variant={"query"}  >
           <Badge className="flex">Question</Badge>
